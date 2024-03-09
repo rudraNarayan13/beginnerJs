a8d0ff45380d95ee33493983aed8954e7e0c70d9
Rudra narayan Behera <104723070+rudraNarayan13@users.noreply.github.com>
2024-03-09 21:25:53 +0530

    Add files via upload

---
+++ controlFlow.js
@@ -0,0 +1,31 @@
+// How to ontrol the code work and the chronology 
+
+
+// IF ELSE , switch case , Nullish coaliscing ( ?? ), Terniary operator (?  :), [ < ,> ,<= ,>= ]
+// if (2===3){
+//     console.log(`true code is running `);
+// }
+// else if(1==="1"){
+//     console.log(`elseIf code is running `);
+// }else{
+//     console.log(`Else code is running`);
+// }
+
+// && : and , || :Or ( to check multiple conditions );
+
+
+//-------------------------IMPORTANT-----------------------------------//
+// Nullish coaliscing ( ?? ) vs Terniary operator (?  :)
+
+
+// ## nullish coalscing = To define if thjere is a null or undefined value returning from any function or event then how to react 
+// for example :: var abc = null ?? 10 ; This staement will always print 10 by ignoring null value .same with undefined . 
+// let v = null ?? 100 ;
+// console.log(`value of V is ${v}`);
+
+
+// ## Terminary Operator : In this opertpr we first check the condition and then on the base of result we command waht to run .
+// Syntax: condition ? if true run this : if false run this ;
+// for example ::let a = 10 ;  a>=10 ? console.log(`value of a is ${a}`): console.log(`false code runing `);
+
+
---
+++ dom.js
@@ -0,0 +1,53 @@
+// How to catch elements and attributes ? How to read edit inpu delete data in document?
+// QuerySelector 
+// appendChild
+// Parent-child-sibling relation 
+// Data extraction from dataBase and inject in document 
+// ****CSS selectors ****      
+
+
+// What is DOM? 
+// ===============
+// The DOM represents the document as nodes and objects; that way, programming languages can interact with the page.Document Object Model (DOM) representation allows webpage or HTML source to be manipulated .The DOM is built using multiple APIs that work together.
+//     The DOM is not a programming language, but without it, the JavaScript language wouldn't have any model or notion of web pages, HTML documents, SVG documents, and their component parts. 
+//*DOM is not part of Js* .The document as a whole, the head, tables within the document, table headers, text within the table cells, and all other elements in a document are parts of the document object model for that document. They can all be accessed and manipulated using the DOM and a scripting language like JavaScript.
+
+
+// run this function when the document is loaded
+window.onkeypress = (e) => {
+    // create a couple of elements in an otherwise empty HTML page
+    const heading = document.createElement("h1");// header space created like this <h1></h1>
+    const headingText = document.createTextNode("Big Head!");// text writeen on memory.. not injected yet in document
+    heading.appendChild(headingText);// being text added to h1 tag 
+    document.body.appendChild(heading);// being h1 tag added to the document
+    heading.id="head";
+  //   headingText.replaceWith("hello");
+    console.log(`operation 1's data :- ${e}`);
+  };
+  window.onclick = (e) =>{
+      const h = document.getElementById(`head`)
+    //   h.replaceWith(`Header has been changed on click`)
+    h.remove()
+      console.log(e);// in this function we are getting data of mouse click event
+      
+  }
+//  const heading = document.querySelector("#header")
+//  heading.innerHTML="hello"
+//   const headingText = document.createTextNode("DomManupulationTutorial");
+//   heading.appendChild(headingText);
+window.onload = (
+    (e)=>{
+        const titel =document.createElement("title");
+        const titelText= document.createTextNode("DOM MANIPULATION 1")
+        document.head.appendChild(titel);
+        titel.appendChild(titelText);
+        console.log(e);
+        // console.log(`Take time to load:${e.timeStamp}ms`);
+        const divOne= document.createElement("div");
+        const divText=document.createTextNode(`Take time to load:${e.timeStamp}ms`);
+        document.body.appendChild(divOne);
+        divOne.appendChild(divText);
+        
+
+    }
+)
