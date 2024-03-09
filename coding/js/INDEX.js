b47f0b51b6e631b8f72effe157e845b3a0262af7
Rudra narayan Behera <104723070+rudraNarayan13@users.noreply.github.com>
2024-03-09 16:35:04 +0000

    basic topics of js notes

---
+++ coding/js/INDEX.js
@@ -0,0 +1,39 @@
+// // data types in js
+// //=====================
+// // 1.number 
+// // 2.Boolean= YES OR NO  
+// // 3.String
+// // 4.null = it is a stand alone value . it is a special type .
+// // 5.undefined= its a value 
+// //6.Symbol= mostly used in react fir unique 
+
+
+// // object = 
+// //arr=
+// // CONVERSION
+// // let score = "33";
+// // console.log(typeof score);
+// // console.log(typeof (score));
+// // let valueInNumber = Number(score);
+// // console.log(typeof valueInNumber);
+// // let valInNumber = Boolean(score);
+// // console.log(typeof valInNumber);
+// // console.log(typeof NaN);
+// let val1 = "";
+// let val2 = 100
+// console.log(val1>val2);
+// //false
+// console.log(val1=val2);
+// //value of val2
+// console.log(val1<val2);
+// //false
+// console.log(typeof val1 , typeof val2);
+// // num num 
+
+// //memory
+// // 2 types == 1. stack 2. heap
+// // stack = primitive type mempry 
+// // heap = no primitive type [], {}, ()=>{} 
+// // in stack memory a copy of original dat is provided .. so any changes done .. that done on copied data , original data is safe . 
+// // for example 
+// let and const dont follow the hoistion rule //
---
+++ coding/js/iteration.js
@@ -0,0 +1,34 @@
+//  Loops AKA itearators { for ,do while, break continue }
+
+//For loops 
+// let arr = [ "a","b","c","d" ]
+// for (let index = 0; index < arr.length; index++) {
+    // if(arr.length>2){
+    //     break
+    // }
+//     const element = arr[index];
+//     const indexIs= arr.indexOf(element);
+//     console.log(`element is ${element}`);
+//     console.log(`index of  ${element} is ${indexIs}`);
+// }
+
+// for (let h= 0 ; h < 10; h++){
+//     const abc = h ;
+//     console.log(`${abc}`);
+//     if(h == 5 ){ 
+//         console.log(`5 is the best`);}
+// }
+// console.log(`hello`)
+
+// ==================== BREAK Concept ==================
+// for (let a = 1; a <=25; a++) {
+//     if(a<11 ){
+//         break
+//     }
+//     console.log(`Table of ${a}`);
+//     for (let j = 1; j <= 10; j++) {
+       
+//         console.log(`${a} * ${j} = ${a*j} `  ) ;
+        
+//     }
+// }
